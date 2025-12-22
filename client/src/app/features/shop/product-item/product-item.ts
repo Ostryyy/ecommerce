9d96156cdef4bab71d8a7ad5f2d69e45c1f82dbc
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2025-12-23 00:32:38 +0100

    feat(client): add routing, nav links, and product details page

--- client/src/app/features/shop/product-item/product-item.ts
+++ client/src/app/features/shop/product-item/product-item.ts
@@ -4,10 +4,11 @@ import { MatCard, MatCardContent, MatCardActions } from '@angular/material/card'
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-product-item',
-  imports: [MatCard, MatCardContent, MatButton, CurrencyPipe, MatIcon, MatCardActions],
+  imports: [MatCard, MatCardContent, MatButton, CurrencyPipe, MatIcon, MatCardActions, RouterLink],
   templateUrl: './product-item.html',
   styleUrl: './product-item.scss',
 })
