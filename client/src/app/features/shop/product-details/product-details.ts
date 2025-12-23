e2a3cb73ae2df0c29dda8b4010a276fdef85d31e
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2025-12-23 21:12:25 +0100

    feat(client): add shopping cart, checkout flow, and app initialization

--- client/src/app/features/shop/product-details/product-details.ts
+++ client/src/app/features/shop/product-details/product-details.ts
@@ -7,18 +7,33 @@ import { MatAnchor, MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import { MatDivider } from "@angular/material/divider";
+import { MatDivider } from '@angular/material/divider';
+import { CartService } from '../../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CurrencyPipe, MatAnchor, MatButton, MatIcon, MatFormField, MatInput, MatLabel, MatDivider],
+  imports: [
+    CurrencyPipe,
+    MatAnchor,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDivider,
+    FormsModule,
+  ],
   templateUrl: './product-details.html',
   styleUrl: './product-details.scss',
 })
 export class ProductDetails implements OnInit {
   private shopService = inject(ShopService);
+  private cartService = inject(CartService);
   private activatedRoute = inject(ActivatedRoute);
   product?: Product;
+  quantityInCart = 0;
+  quantity = 1;
 
   ngOnInit() {
     this.loadProduct();
@@ -28,8 +43,35 @@ export class ProductDetails implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (!id) return;
     this.shopService.getProduct(+id).subscribe({
-      next: (product) => (this.product = product),
-      error: (error) => console.error('There was an error!', error),
+      next: (product) => {
+        this.product = product;
+        this.updateQuantityInBasket();
+      },
+      error: (error) => console.log(error),
     });
+  }
+
+  updateCart() {
+    if (!this.product) return;
+    if (this.quantity > this.quantityInCart) {
+      const itemsToAdd = this.quantity - this.quantityInCart;
+      this.quantityInCart += itemsToAdd;
+      this.cartService.addItemToCart(this.product, itemsToAdd);
+    } else {
+      const itemsToRemove = this.quantityInCart - this.quantity;
+      this.quantityInCart -= itemsToRemove;
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove);
+    }
+  }
+
+  updateQuantityInBasket() {
+    this.quantityInCart =
+      this.cartService.cart()?.items.find((item) => item.productId === this.product?.id)
+        ?.quantity || 0;
+    this.quantity = this.quantityInCart || 1;
+  }
+
+  getButtonText() {
+    return this.quantityInCart > 0 ? 'Update Cart' : 'Add to Cart';
   }
 }
---
+++ client/src/app/shared/components/order-summary/order-summary.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatAnchor } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { CartService } from '../../../core/services/cart.service';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-order-summary',
+  imports: [RouterLink, MatAnchor, MatFormField, MatLabel, MatInput, CurrencyPipe],
+  templateUrl: './order-summary.html',
+  styleUrl: './order-summary.scss',
+})
+export class OrderSummary {
+  cartService = inject(CartService);
+}
